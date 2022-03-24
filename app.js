377507a5af0a8966f47b087ad0eeb42c2a69b398
Andre Bamidele <a.bamidele@ossila.com>
2022-03-24 10:02:48 +0000

    added tc routing

--- app.js
+++ app.js
@@ -1,117 +1,116 @@
 /*
-    * Author: Bernoulli Mukuna
-    * created: 10/05/2020
-*/
+ * Author: Bernoulli Mukuna
+ * created: 10/05/2020
+ */
 
-require('dotenv').config({path: `${__dirname}\\.env`})
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var flash = require('connect-flash');
-var session = require('express-session');
+require("dotenv").config({ path: `${__dirname}\\.env` });
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var flash = require("connect-flash");
+var session = require("express-session");
 var MongoStore = require("connect-mongo");
-var passport = require('passport');
-var favicon = require('serve-favicon');
+var passport = require("passport");
+var favicon = require("serve-favicon");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var serviceRouter = require('./routes/services');// This path should not exist. It will go
-var aboutRouter = require('./routes/aboutUs');
-var accountRouter = require('./routes/account');
-var policyRouter = require('./routes/policy');
-var bookingRouter = require('./routes/booking').router;
-var messageRouter = require('./routes/sever-side-messages').router;
-var paymentRouter = require('./routes/payment').router;
-var cloudapi = require('./routes/cloudapi');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var serviceRouter = require("./routes/services"); // This path should not exist. It will go
+var aboutRouter = require("./routes/aboutUs");
+var termsRouter = require("./routes/terms");
+var accountRouter = require("./routes/account");
+var policyRouter = require("./routes/policy");
+var bookingRouter = require("./routes/booking").router;
+var messageRouter = require("./routes/sever-side-messages").router;
+var paymentRouter = require("./routes/payment").router;
+var cloudapi = require("./routes/cloudapi");
 
 var app = express();
 
 // Passport Config
-require('./bin/passport-config')(passport);
+require("./bin/passport-config")(passport);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "/../public")));
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
 // Middlewares
-app.use(logger('dev'));
-app.use('/payment/webhook', express.raw({type: "*/*"})) // stripe payment middleware
+app.use(logger("dev"));
+app.use("/payment/webhook", express.raw({ type: "*/*" })); // stripe payment middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(__dirname + "/public/favicon.ico"));
 
 // Express Session Settings
 let cookieExpirationTime = parseInt(process.env.COOKIE_EXPRESSION_TIME); // session expires after 90 days
-let sessionDB_name = 'kingsHire_sessions';
+let sessionDB_name = "kingsHire_sessions";
 
 let sessionStore = MongoStore.create({
-    mongoUrl: process.env.REMOTE_MONGO_URI,
-    dbName: 'kingsHireDatabase',
-    collectionName: sessionDB_name
+  mongoUrl: process.env.REMOTE_MONGO_URI,
+  dbName: "kingsHireDatabase",
+  collectionName: sessionDB_name,
 });
 
 app.use(
-    session({
-      secret: process.env.SESSION_SECRET_ID,
-      store: sessionStore,
-      resave: true,
-      saveUninitialized: false,
-      rolling: true,
-      cookie: { expires : new Date(Date.now() + cookieExpirationTime)}
-    })
+  session({
+    secret: process.env.SESSION_SECRET_ID,
+    store: sessionStore,
+    resave: true,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: { expires: new Date(Date.now() + cookieExpirationTime) },
+  })
 );
 
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session()); // Keep track of the user session
 
-
 // Connect flash
 app.use(flash());
 
 // Global Variables
 app.use(function (req, res, next) {
-
   // Available only to the view(s) rendered during that request/response cycle
-  res.locals.success_message = req.flash('success_message');
-  res.locals.error_message = req.flash('error_message');
-  res.locals.error = req.flash('error');
+  res.locals.success_message = req.flash("success_message");
+  res.locals.error_message = req.flash("error_message");
+  res.locals.error = req.flash("error");
   next();
 });
 
-app.use('/', indexRouter); 
-app.use('/users', usersRouter);
-app.use('/account', accountRouter);
-app.use('/join', indexRouter);// using the same route
-app.use('/login', indexRouter); //
-app.use('/about-us', aboutRouter);
-app.use('/services', serviceRouter);
-app.use('/policy', policyRouter);
-app.use('/booking', bookingRouter);
-app.use('/messages', messageRouter);
-app.use('/payment', paymentRouter);
-app.use('/cloudapi', cloudapi);
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/account", accountRouter);
+app.use("/join", indexRouter); // using the same route
+app.use("/login", indexRouter); //
+app.use("/about-us", aboutRouter);
+app.use("/terms", termsRouter);
+app.use("/services", serviceRouter);
+app.use("/policy", policyRouter);
+app.use("/booking", bookingRouter);
+app.use("/messages", messageRouter);
+app.use("/payment", paymentRouter);
+app.use("/cloudapi", cloudapi);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in devendorvelopment
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/cloudapi.js
+++ routes/cloudapi.js
@@ -6,19 +6,19 @@ var path = require("path");
 var multer = require("multer");
 
 let storage = multer.diskStorage({
-    filename: function (req, file, callback) {
-        callback(null, file.fieldname + "-" + file.originalname);
-    },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + "-" + file.originalname);
+  },
 });
 
 let multerPortfolioImage = multer({
-    storage: storage,
+  storage: storage,
 }).single("portfolio_image");
 
 //Google cloud client
 const gc = new Storage({
-    credentials: JSON.parse(process.env.GCS_KEYFILE),
-    projectId: process.env.GCS_PROJECT_ID,
+  keyFilename: path.join(__dirname, "../kingshire-344704-c468a6ed0dcd.json"),
+  projectId: "kingshire-344704",
 });
 
 const bucketName = "kingshireimages";
@@ -26,98 +26,95 @@ gcUserFiles = gc.bucket(bucketName);
 
 // Upload to cloud
 async function uploadRemoteFile(filePath) {
-    try {
-        let encodedName = emailEncode(filePath) + path.extname(filePath);
-        await gc.bucket(bucketName).upload(filePath, {
-            destination: encodedName,
-        });
-        console.log(`${filePath} uploaded to ${bucketName}`);
-    }catch (error) {
-        throw error;
-    }
-
+  try {
+    let encodedName = emailEncode(filePath) + path.extname(filePath);
+    await gc.bucket(bucketName).upload(filePath, {
+      destination: encodedName,
+    });
+    console.log(`${filePath} uploaded to ${bucketName}`);
+  } catch (error) {
+    throw error;
+  }
 }
 
 // Delete Function on cloud
 async function deleteRemoteFile(req) {
-    try {
-        let url = req.body["current_src"];
-        var portfolio_image = req.user.portfolio.filter(function (value) {
-            return value.pictureURL === url;
-        })[0];
+  try {
+    let url = req.body["current_src"];
+    var portfolio_image = req.user.portfolio.filter(function (value) {
+      return value.pictureURL === url;
+    })[0];
 
-        fileName = portfolio_image.name + portfolio_image.fileType;
+    fileName = portfolio_image.name + portfolio_image.fileType;
 
-        await gc.bucket(bucketName).file(fileName).delete();
-
-    }catch (error) {
-        throw error
-    }
+    await gc.bucket(bucketName).file(fileName).delete();
+  } catch (error) {
+    throw error;
+  }
 }
 
 // Update db
 function addToDB(req, res) {
-    let user = req.user;
-    let filePath = req.file.path;
-    let description = req.body["image_info"];
-    let encodedPath = emailEncode(filePath);
-    let encodedName = encodedPath + path.extname(filePath);
+  let user = req.user;
+  let filePath = req.file.path;
+  let description = req.body["image_info"];
+  let encodedPath = emailEncode(filePath);
+  let encodedName = encodedPath + path.extname(filePath);
 
-    user.portfolio.push({
-        pictureURL: "https://storage.cloud.google.com/kingshireimages/" + encodedName,
-        description: description,
-        name: encodedPath,
-        fileType: path.extname(filePath),
-    });
+  user.portfolio.push({
+    pictureURL:
+      "https://storage.cloud.google.com/kingshireimages/" + encodedName,
+    description: description,
+    name: encodedPath,
+    fileType: path.extname(filePath),
+  });
 
-    user.save((err) => {
-        if (err) {
-            throw err;
-        }
-        console.log("Portfolio image Sent to DB successfully!");
-        req.flash("success_message", 'Portfolio Successfully updated');
-        res.redirect(`back`);
-
-    });
+  user.save((err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("Portfolio image Sent to DB successfully!");
+    req.flash("success_message", "Portfolio Successfully updated");
+    res.redirect(`back`);
+  });
 }
 
 function deleteFromDB(req, res) {
-    let url = req.body["current_src"];
-    var portfolio_image = req.user.portfolio.filter(function (value) {
-        return value.pictureURL === url;
-    })[0];
+  let url = req.body["current_src"];
+  var portfolio_image = req.user.portfolio.filter(function (value) {
+    return value.pictureURL === url;
+  })[0];
 
-    req.user.portfolio.pull({ _id: portfolio_image._id });
+  req.user.portfolio.pull({ _id: portfolio_image._id });
 
-    req.user.save((err) => {
-        if (err) {
-            console.log(err)
-            throw err;
-        }
-        console.log("Portfolio image deleted from DB successfully!");
-        req.flash("success_message", 'Portfolio Successfully Deleted');
-        res.redirect(`back`);
-    });
+  req.user.save((err) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    console.log("Portfolio image deleted from DB successfully!");
+    req.flash("success_message", "Portfolio Successfully Deleted");
+    res.redirect(`back`);
+  });
 }
 
 // Routes
 router.post("/upload", multerPortfolioImage, function (req, res, next) {
-    try {
-        uploadRemoteFile(req.file.path).catch(console.error);
-        addToDB(req, res);
-    }catch (error) {
-        next(error);
-    }
-
+  try {
+    uploadRemoteFile(req.file.path).catch(console.error);
+    addToDB(req, res);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/delete", multerPortfolioImage, function (req, res, next) {
-    try {
-        deleteRemoteFile(req);
-        deleteFromDB(req, res);
-    }catch (error) {
-        next(error)
-    }
+  try {
+    deleteRemoteFile(req);
+    deleteFromDB(req, res);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
