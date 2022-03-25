1c89bd3e7aac62e4c7d27ff3f78c60c1c189b6f5
Chisom7 <68484036+Chisom7@users.noreply.github.com>
2022-03-25 18:17:47 +0000

    fix: mobile header fixed

--- KingsHireOld/public/javascripts/socketio-connection-client-side.js
+++
@@ -1,7 +0,0 @@
-let dev_URL = 'http://localhost:3000';
-let prod_URL = 'https://kingshire.herokuapp.com/'
-//var socket = io.connect(dev_URL);
-var socket = io.connect(dev_URL, {secure: true});
-
-export{socket, dev_URL, prod_URL}
-
